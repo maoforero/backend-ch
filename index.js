be800b9e49feadd0f4181846777398c92837dca6
maoforero <maforeror@gmail.com>
2021-12-21 02:22:38 -0500

    trabajando en CR

--- index.js
+++ index.js
@@ -4,63 +4,46 @@ let { Router } = express;
 let routerPro = new Router;
 let routerCar = new Router;
 
-//Productos
+//Puerto
+const PORT = 8080;
 
 let productos = [];
 let carrito = [];
 
-//CRUD Productos
+//######## CRUD Productos
 //Get productos
 routerPro.get("/", (req, res, next) => {
     res.json(productos);
+})
+// Post productos
+routerPro.post("/", (req, res, next) => {
+    productos.push(req.query.add)
+    res.json(productos);
 });
 
-//Post productos
-routerPro.post("/", (req, res, next) => {
-    productos.push(req.query.agregar)
-    res.json(productos);
-})
 
-
-//CRUD Carrito
+//####### CRUD Carrito
 //Get Carrito
 routerCar.get("/", (req, res, next) => {
-    res.json(carrito);
+    res.json(carrito)
 });
 
 //Post Carrito
 routerCar.post("/", (req, res, next) => {
-    carrito.push(req.query.listar)
+    carrito.push(req.query.add)
     res.json(carrito);
 });
 
-
-
-
-//Puerto
-const PORT = 8080;
-
-//Router Productos
-routerPro.get("/", (req, res, next) => {
-    res.send("Productos")
-});
-
-//Router Carrito
-routerCar.get("/", (req, res, next) => {
-    res.send("Carrito")
-})
+//Router
+app.use("/productos", routerPro);
+app.use("/carrito", routerCar);
 
 //Router Home
 app.get("/", (req, res, next) => {
     res.send("Home")
 })
 
-
-//Router
-app.use("/producto", routerPro);
-app.use("/carrito", routerCar);
-
 // Aplicacion escuchando y regresando un console log
 app.listen(PORT, () => {
-    console.log(`Listening http://localhost:${PORT}`);
-})
+    console.log(`Listening http://localhost:${PORT} 👽`);
+    })
